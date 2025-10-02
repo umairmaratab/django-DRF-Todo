1b5af9494c244ecf3c852c61753d289ae77a6e1c
Umair Maratab <p180045@nu.edu.pk>
2025-10-02 19:49:07 +0500

    Added REST_FRAMEWORK settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party
+    'rest_framework',
+    # Local
+    'todos.apps.TodosConfig',   # new
 ]
 
 MIDDLEWARE = [
@@ -120,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
