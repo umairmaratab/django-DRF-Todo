992c0c808b256157bb33f910fa64f624dd1dd90c
Umair Maratab <p180045@nu.edu.pk>
2025-10-02 20:25:12 +0500

    Added app urls

---
+++ todos/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import path 
+
+from .views import ListTodo, DetailTodo
+
+urlpatterns = [
+    path("<int:pk>/", DetailTodo.as_view(), name="todo_detial"),
+    path("", ListTodo.as_view(), name="todo_list"),
+]
